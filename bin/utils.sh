94a5a498c8e1a9bd8c94af6d5ed7c21e661699f6
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-04-08 19:21:31 -0500

    shfmt

--- bin/utils.sh
+++ bin/utils.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 echoerr() {
-  echo "$1" >&2
+    echo "$1" >&2
 }
 
 pipenv_bin() {
-  echo "$RTX_INSTALL_PATH/asdf_bin/pipenv"
+    echo "$RTX_INSTALL_PATH/asdf_bin/pipenv"
 }
 
 pipenv_venv() {
-  local pipfile
-  pipfile="$(eval "echo ${RTX_TOOL_OPTS__PIPFILE:-}")"
-  if [ "$pipfile" = "" ]; then
-    return
-  fi
-  if [[ "$pipfile" != /* ]] && [[ -n "${RTX_PROJECT_ROOT:-}" ]]; then
-    pipfile="${RTX_PROJECT_ROOT:-}/$pipfile"
-  fi
-  if [[ ! -f "$pipfile" ]]; then
-    echoerr "rtx-pipenv: no Pipfile found at $pipfile"
-    exit 1
-  fi
-  "$(pipenv_bin)" --venv 2>/dev/null
+    local pipfile
+    pipfile="$(eval "echo ${RTX_TOOL_OPTS__PIPFILE:-}")"
+    if [ "$pipfile" = "" ]; then
+        return
+    fi
+    if [[ "$pipfile" != /* ]] && [[ -n "${RTX_PROJECT_ROOT:-}" ]]; then
+        pipfile="${RTX_PROJECT_ROOT:-}/$pipfile"
+    fi
+    if [[ ! -f "$pipfile" ]]; then
+        echoerr "rtx-pipenv: no Pipfile found at $pipfile"
+        exit 1
+    fi
+    "$(pipenv_bin)" --venv 2>/dev/null
 }
